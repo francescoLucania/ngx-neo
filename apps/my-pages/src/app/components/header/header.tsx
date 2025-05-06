5098809564317d089969c9e1af96070439d3efd1
francescolucaniacom <35537086+francescoLucania@users.noreply.github.com>
2025-05-06 12:07:10 +0300

    add styles for header

--- apps/my-pages/src/app/components/header/header.tsx
+++ apps/my-pages/src/app/components/header/header.tsx
@@ -1,7 +1,7 @@
 import { HeaderMenu } from '../../mocks/menu/menu';
 import PersonalCabinetButton from './components/personal-cabinet-button/personal-cabinet-button';
 import styles from './header.module.scss';
-import Image from 'next/image'
+import Image from 'next/image';
 
 export function Header() {
   const headerMenu = HeaderMenu;
@@ -10,26 +10,24 @@ export function Header() {
     <header className={styles.header + ' ' + 'pages-header'}>
       <div className="container">
         <nav className={styles.header__navigate}>
-
-          <a href='/' className={styles.header__navigate__logo}>
-            <img src='/logo.jpg' alt="" />
+          <a href="/" className={styles.header__navigate__logo}>
+            <img src="/logo.jpg" alt="" />
           </a>
 
-        <ul>
-          {headerMenu.common.map((item, i) => (
-            <li key={i}>
-              <a href={'/' + item.route}>{item.name}</a>
+          <ul>
+            {headerMenu.common.map((item, i) => (
+              <li key={i}>
+                <a href={'/' + item.route}>{item.name}</a>
+              </li>
+            ))}
+            <li>
+              <PersonalCabinetButton />
             </li>
-          ))}
-          <li>
-            <PersonalCabinetButton />
-          </li>
-        </ul>
-      </nav>
-    </div>
-</header>
-)
-  ;
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
